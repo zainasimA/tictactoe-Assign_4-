9703cd42dbcb52b6c3436f62fe52e168b9710218
zainasimA <45763251+zainasimA@users.noreply.github.com>
2019-03-24 00:23:52 +0500

    Add files via upload

---
+++ AP_Assign.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ap_assign;
+
+/**
+ *
+ * @author hp
+ */
+public class AP_Assign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Controller c = new Controller();
+        one o = new one();
+    }
+    
+}
